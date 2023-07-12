5493bf4a42d6a14116b58cf0854a792698ee76ed
Chelo Tasnim <117082277+CheloTasnim@users.noreply.github.com>
2023-07-12 09:41:08 +0700

    Delete Duplicating Directory

--- ekstrakurikuler/admin/deleteAdmin.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include "../connect.php";
-    $id = $_GET['id'];
-    $delete = mysqli_query($conn, "DELETE FROM user WHERE id = '$id'");
-    if($delete){
-        header("Location:index.php");
-    }
-?>
--- ekstrakurikuler/delete/deleteCard.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include "../connect.php";
-    $id = $_GET['id'];
-    $delete = mysqli_query($conn,"DELETE FROM pesan WHERE id = '$id'");
-    if($delete){
-        header("Location: ../admin/index.php?halaman=report");
-    }
-?>
